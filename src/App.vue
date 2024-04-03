8b5ab58a7dee0f8df0cd4aa426e5ae75c1884647
AzizbekDev <azizbek_muzaffarov@mail.ru>
2024-04-03 21:18:53 +0500

    Add Vue composition API and fetch transactions from API

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,37 @@
+<script setup>
+import { ref, onMounted, computed } from 'vue'
+import axios from 'axios'
+
+import Header from './components/Header.vue'
+import Balance from './components/Balance.vue'
+import IncomeExpense from './components/IncomeExpense.vue'
+import TransactionList from './components/TransactionList.vue'
+import AddTransaction from './components/AddTransaction.vue'
+
+const transactions = ref([])
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get('https://26434691557145f7.mokky.dev/transactions')
+    transactions.value = data
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+// Get Total
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => acc + transaction.amount, 0)
+})
+
+</script>
+
 <template>
-  My App
+  <Header />
+  <div class="container">
+    <Balance :total="total" />
+    <IncomeExpense />
+    <TransactionList :transactions="transactions" />
+    <AddTransaction />
+  </div>
 </template>
